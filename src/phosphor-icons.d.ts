8d23f28cd843aa11e30b5915e2d3ee370c13c222
mohamed.bougrioua <mohamed.bougrioua@beyn.io>
2026-02-07 11:39:09 +0100

    commit $

---
+++ src/phosphor-icons.d.ts
@@ -0,0 +1,7 @@
+// This file tells TypeScript to stop complaining about missing types for Phosphor Icons
+declare module '@phosphor-icons/web/regular';
+declare module '@phosphor-icons/web/fill';
+declare module '@phosphor-icons/web/light';
+declare module '@phosphor-icons/web/thin';
+declare module '@phosphor-icons/web/bold';
+declare module '@phosphor-icons/web/duotone';
